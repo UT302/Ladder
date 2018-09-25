826db83acc6740cfc31474a7bb98a29bb5b96cc6
UT302 <nikab.94@gmail.com>
2018-09-25 19:26:48 +0300

    Some comments

--- LadderCreator.py
+++ LadderCreator.py
@@ -15,20 +15,20 @@ f.write(f"Substance/Medecine name: {sn}\n")
 f.write("-" * 25)
 
 f.write(f"\n\nLadder created: {date}")
-cud = "Current Usage Dose (Daily): "
+cud = "Current Usage Dose (Daily): "  #Write the amount without weight units like mg or g
 cudin = input(cud)
 f.write(f"\n{cud}{cudin}")
 dla = "Dosage Lowering Amount: "
-dlain = input(dla)
+dlain = input(dla)                    #Amount by which dose will be decreased each step
 f.write(f"\n{dla}{dlain}")
 dlp = "Dosage Lowering Period (Days): "
-dlpin = input(dlp)
+dlpin = input(dlp)                    #How much days between amount being decreased should pass
 f.write(f"\n{dlp}{dlpin}\n\n")
 
 
 print(f"Creating ladder for {sn}...")
 
-while float(cudin) != 0:
+while float(cudin) != 0:              #Until dosage = 0, keep incrementing data points
     cudin = float(cudin) - float(dlain)
     date = date + datetime.timedelta(days = int(dlpin))
     f.write("=" * 25)
